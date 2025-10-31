ff8f78c33f4893ebafc0922dedaec57c07032eff
Brian Bureson <brian.bureson@gmail.com>
2025-10-31 16:44:16 -0600

    style: add 16px more top padding to bring content down
    Increased hero top padding by 16px:
    - Mobile: pt-20 (from pt-16) - adds 16px
    - Desktop: md:pt-32 (from md:pt-28) - adds 16px
    - Moves page content down by 16px for better positioning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3105,7 +3105,7 @@ I designed the first touch screen insulin pump interface, revolutionizing how pe
     <div className="min-h-screen relative">
 
       {/* Hero Section */}
-      <section className="min-h-screen flex flex-col items-center justify-center px-6 pt-16 md:pt-28 pb-28 md:pb-36">
+      <section className="min-h-screen flex flex-col items-center justify-center px-6 pt-20 md:pt-32 pb-28 md:pb-36">
         
         <motion.div
           initial={{ opacity: 0, y: 30 }}
