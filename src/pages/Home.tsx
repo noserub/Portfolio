dc32fd4dc5541bcc8ffd7eb0c7b84ae3f9532c57
Brian Bureson <brian.bureson@gmail.com>
2025-10-31 21:11:30 -0600

    style: add 4px back to top padding for fine adjustment
    Added 4px back to hero top padding:
    - Mobile: pt-[80px] (from pt-[76px]) - 4px increase
    - Desktop: md:pt-[128px] (from md:pt-[124px]) - 4px increase
    - Moves content down just a tiny bit as requested

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3105,7 +3105,7 @@ I designed the first touch screen insulin pump interface, revolutionizing how pe
     <div className="min-h-screen relative">
 
       {/* Hero Section */}
-      <section className="min-h-screen flex flex-col items-center justify-center px-6 pt-[76px] md:pt-[124px] pb-28 md:pb-36">
+      <section className="min-h-screen flex flex-col items-center justify-center px-6 pt-[80px] md:pt-[128px] pb-28 md:pb-36">
         
         <motion.div
           initial={{ opacity: 0, y: 30 }}
